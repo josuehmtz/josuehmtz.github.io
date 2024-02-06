fce681f02866541543d56d8348c5c24932c0cb21
Hiram Alvarez <hiram.alvarez@workteam.com.mx>
2024-02-06 17:35:00 -0600

    react project

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { NavBar } from './components'
+
+
+function App() {
+
+  return (
+    <>
+      <NavBar/>
+    </>
+  )
+}
+
+export default App
